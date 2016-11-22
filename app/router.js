ac73fe422cd9a2ce9c3e4470f2fda38586cf44e1
stephhappens <stephanie.n.provence@gmail.com>
2016-11-22 16:27:34 -0600

    finish dynamic routes

--- app/router.js
+++ app/router.js
@@ -9,9 +9,14 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('admin', function() {
     this.route('category', function() {
+      // Creates a new... Menu Category
       this.route('new');
+
+      // Shows the detail page for Menu Category with `id` detail_id
       this.route('detail', { path: '/:detail_id'});
-      this.route('new-item');
+
+      // Shows the form to create a new Menu Item for Menu Category with `id` menu-category_id
+      this.route('new-item', { path: '/:menu-category_id/new-item'});
     });
   });
 });
